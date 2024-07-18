906d955371ebb9cd11d709a3763b411f0412ca6c
varakiVakeesan <varakivakeesan23@gmail.com>
2024-07-18 12:21:03 +0530

    UI Changes in home page

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
-import FloatContent from '../Components/Float';
-import RoleSelectionComponent from '../Components/RoleSelectionComponent';
-import Navigation from '../Components/Navigation';
-import NavBarComponent from '../Components/NavigationBar';
+import React, { useState, useEffect } from "react";
+import FloatContent from "../Components/Float";
+import RoleSelectionComponent from "../Components/RoleSelectionComponent";
+import Navigation from "../Components/Navigation";
+import NavBarComponent from "../Components/NavigationBar";
+
 const HomePage = () => {
-    return ( <div>
-        <NavBarComponent/>
-        <FloatContent/>
-        <RoleSelectionComponent/>
-        <Navigation/>
+  const [loginStatus, setLoginStatus] = useState(
+    localStorage.getItem("loginSuccess") === "true"
+  );
 
+  useEffect(() => {
+    const handleStorageChange = () => {
+      setLoginStatus(localStorage.getItem("loginSuccess") === "true");
+    };
+
+    window.addEventListener("storage", handleStorageChange);
+
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+    };
+  }, []);
+
+  return (
+    <div>
+      <NavBarComponent />
+      <FloatContent />
+      {loginStatus ? null : <RoleSelectionComponent />}
+      {/* <Navigation/> */}
     </div>
+  );
+};
 
-     );
-}
- 
 export default HomePage;
